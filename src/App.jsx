4cc06d41f4a2b7784d62a1fd24bffe2e92f9f4e3
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-06-01 22:10:33 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { Navigate, BrowserRouter, Routes, Route } from "react-router";
+
+
+function App() {
+  
+
+  return (
+    <>
+    <p className=" font-mono bg-red-400 text-4xl p-5 rounded text-center">Hello World!</p>
+      
+    </>
+  )
+}
+
+export default App

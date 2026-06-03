88d8f80749776cbb39554c93a2e47481b250f202
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-06-03 23:08:56 +0100

    wrapped app in authProvider context

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,14 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './style.css'
+import { AuthProvider } from "./context/authContext";
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <AuthProvider>
     <App />
+    </AuthProvider>
   </StrictMode>,
 )

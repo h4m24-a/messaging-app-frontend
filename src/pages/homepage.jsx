c55551a6cdeab3ee63647439f99003a406e5f1d7
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-06-07 01:10:55 +0100

    set up state to manage id for active conversation

--- src/pages/homepage.jsx
+++ src/pages/homepage.jsx
@@ -2,14 +2,22 @@ import Navigation from "../components/navigation";
 import ConversationList from "../components/conversationList";
 import ChatWindow from "../components/chatWindow";
 import NewConversationModal from "../components/NewConversationModal";
+import { useState } from "react";
 
 export default function Homepage() {
+
+  const [activeConversationId, setActiveConversationId] = useState(null)
+
   return (
     <div className="flex h-screen bg-slate-100 font-PlusJakarta p-8">
       <div className="flex flex-1 overflow-hidden rounded-3xl   bg-white shadow-xl">
         <Navigation />
-        <ConversationList />
-        <ChatWindow />
+        <ConversationList
+          activeId={activeConversationId}
+          onSelectConversation={setActiveConversationId}
+        
+        />
+        <ChatWindow key={activeConversationId} conversationId={activeConversationId} />
       </div>
 
       <div className="ml-8">

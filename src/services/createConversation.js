7073ed565e13286ed4da4b58b2cb538a6dbeae2f
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-06-09 23:10:50 +0100

    function to create a new conversation

---
+++ src/services/createConversation.js
@@ -0,0 +1,25 @@
+const createConversation = async (token, userBId ) => {
+  const response = await fetch("http://localhost:3000/conversations/create", {
+    method: 'POST',
+    credentials: 'include',
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`,
+    },
+    body: JSON.stringify({ userBId })
+  });
+
+  const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.error);
+  }
+
+  console.log(data)
+  return data;
+};
+
+
+
+
+export default createConversation;

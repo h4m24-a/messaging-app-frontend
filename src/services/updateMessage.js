080e1a3ec89f45e0209da5393fa0b01822c1e020
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-06-12 23:02:23 +0100

    function to update message

---
+++ src/services/updateMessage.js
@@ -0,0 +1,26 @@
+const updateMessage = async (token, conversationId, updatedMessage, messageId) => {
+  const response = await fetch(`http://localhost:3000/conversations/${conversationId}/messages/${messageId}`, {
+    method: 'PUT',
+    credentials: 'include',
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${token}`
+    },
+    body: JSON.stringify({ updatedMessage })
+  });
+
+    const data = await response.json();
+
+  if (!response.ok) {
+    throw new Error(data.error || 'Failed to update message');
+  }
+
+  return {
+    status: response.status,
+    data
+  };
+
+}
+
+
+export default updateMessage

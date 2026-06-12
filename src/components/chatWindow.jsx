2e8907241d6826eaf13dc5a5299b66a572804a2f
Hamza Ashraf <hamza.ashraf@live.co.uk>
2026-06-12 23:02:46 +0100

    set up query and form to update a message

--- src/components/chatWindow.jsx
+++ src/components/chatWindow.jsx
@@ -2,7 +2,8 @@ import { useQuery, useQueryClient } from "@tanstack/react-query";
 import { useAuthContext } from "../context/useAuthContext";
 import viewConversation from "../services/viewConversation";
 import createMessage from "../services/createMessage";
-import { useState } from "react";
+import updateMessage from "../services/updateMessage";
+import { useState, useEffect, Fragment } from "react";
 
 export default function ChatWindow({ conversationId }) {
 
@@ -11,6 +12,14 @@ export default function ChatWindow({ conversationId }) {
   const [message, setMessage] = useState("");
   const [messageError, setMessageError] = useState("")
   const [messageValidationError, setMessageValidationError] = useState("")
+
+  const [updatedMessage, setUpdatedMessage] = useState("");
+  const [updatedMessageError, setUpdatedMessageError] = useState("")
+  const [updatedMessageValidationError, setUpdatedMessageValidationError] = useState("")
+
+  const [selectMessageId, setSelectMessageId] = useState()
+
+  const [showForm, setShowForm] = useState(false)
 
 
 const { accessToken, userId } = useAuthContext();
@@ -21,6 +30,27 @@ const { accessToken, userId } = useAuthContext();
     queryFn: () => viewConversation(accessToken, conversationId ),
     enabled: !!accessToken && !!conversationId
   })
+
+    useEffect(() => {
+    // clear updatedComment input when selected id changes
+    setUpdatedMessage("")
+    setUpdatedMessageError("")
+    setUpdatedMessageValidationError("")
+  }, [selectMessageId])  // This hook runs whenever selectCommentId changes
+  
+
+
+
+  // Populate input form with comment for update form
+  useEffect(() => {
+    const selectedMessage = data?.conversation?.messages.find(msg => msg.id === selectMessageId);
+    
+    if (selectedMessage) {
+      setUpdatedMessage(selectedMessage.text);  // Populate form with selected comment's content
+    } else {
+      setUpdatedMessage("")
+    }
+  }, [selectMessageId, data])
 
 
   const convo = data?.conversation
@@ -60,6 +90,54 @@ const { accessToken, userId } = useAuthContext();
     } catch (error) {
       setMessageError(error.message)
     }
+  }
+
+
+
+  // Updating message
+  const HandlePutMessage = async (e, messageId) => {
+    try {
+      e.preventDefault()
+  
+      const response = await updateMessage(accessToken, conversationId, updatedMessage, messageId)
+  
+      // Validation errors
+      if (response.status === 400) {
+  
+      const updateMessageErr = {}
+  
+      // Convert error into an object
+      response.data.errors.forEach(err => {
+        updateMessageErr[err.path] = err.msg
+      })
+  
+      setMessageValidationError(updateMessageErr)
+      setUpdatedMessage("")
+      setUpdatedMessageError("")
+      queryClient.invalidateQueries({ queryKey: ['conversation']})
+    }
+    
+    if (response) {
+      setUpdatedMessageError("")
+      setUpdatedMessage("")
+      setMessageValidationError("")
+      setShowForm(!showForm)
+      queryClient.invalidateQueries({ queryKey: ['conversation']})
+    }
+      
+    } catch (error) {
+      setUpdatedMessageError(error.message)
+    }
+  }
+
+
+  
+// get the id of selected message and use it to display the update form.
+  const HandleSelectMessageId = (messageId) => {
+    setSelectMessageId(messageId)
+    setShowForm(!showForm)    // flips value when clicked to hide and display form
+    setUpdatedMessageError("");
+    setUpdatedMessageValidationError("")
   }
 
 
@@ -110,18 +188,75 @@ const { accessToken, userId } = useAuthContext();
 
         {data?.conversation?.messages.map((msg) => {
           const incoming = msg.sender.id !== userId;
+
           return (
+            <Fragment key={msg.id}>
+              <MessageBubble
+                message={msg.text}
+                incoming={incoming}
+                seen={msg.seen}
+              />
 
-            <MessageBubble
-            key={msg.id}
-            message={msg.text}
-            incoming={incoming} 
-            seen={msg.seen}
-            />
-            
-          )
+              {msg.sender.id === userId && (
+                <div className=" flex flex-row justify-end items-center  gap-3">
+                  <button
+                    type="button"
+                    // onClick={() => HandleDeleteComment(msg.id)}
+                    className="h-8 text-xs cursor-pointer max-w-fit w-fit rounded-md outline-neutral-950 outline px-5 font-medium text-black transition active:scale-110"
+                  >
+                    <i className="fa-solid fa-xmark"></i>
+                  </button>
 
-      })}
+                  <button
+                    type="button"
+                    onClick={() => HandleSelectMessageId(msg.id)}
+                    className="h-8 text-xs cursor-pointer max-w-fit w-fit rounded-md bg-neutral-950 px-5 font-medium text-white transition active:scale-110"
+                  >
+                    <i className="fa-solid fa-pen"></i>
+                  </button>
+                </div>
+              )}
+
+
+              
+
+              {showForm  && selectMessageId === msg.id && (  // Update Message input
+                <footer className="border-t border-slate-300 p-6">
+                <form 
+                onSubmit={(e) => HandlePutMessage(e, msg.id)}
+                >
+                <div className="flex items-center gap-4 rounded-2xl border border-slate-500 px-4 py-3">
+                  <input
+                    placeholder="Type a message..."
+                    className="flex-1 outline-none"
+                    name="updatedMessage"
+                    type="text"
+                    value={updatedMessage}
+                    onChange={(e) => setUpdatedMessage(e.target.value)}
+                    required
+                    />
+
+                  <button type="submit" className="rounded-lg bg-blue-600 p-3 text-white">
+                    <i className="fa-solid fa-paper-plane fa-lg cursor-pointer"></i>
+                  </button>
+                
+              </div>
+                </form>
+                {updatedMessageError && (
+                  <p> {updatedMessageError} </p>
+                )}
+
+                {updatedMessageValidationError.message && (
+                  <p> {updatedMessageValidationError.message} </p>
+                )}
+            </footer>
+
+              )}
+        
+            </Fragment>
+          );
+        })}
+
       </div>
           </>
 
@@ -185,6 +320,10 @@ function MessageBubble({ message, incoming, seen }) {
           Seen
         </p>
       )}
+
+          
+
+
     </div>
   );
 }
